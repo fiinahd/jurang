47b4a3bebdb7ad67b63d8c835e9fae01238fd22e
fiinahd <alfaridzidhanif@gmail.com>
2025-07-08 22:17:29 +0700

    clean

--- backend/app/database.py
+++
@@ -1,5 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient # type: ignore
-from .config import settings
-
-client = AsyncIOMotorClient(settings.MONGO_URI)
-db = client[settings.DATABASE_NAME]
--- backend/app/utils/split_train_eval.py
+++
@@ -1,4 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-def split_data(texts, labels, test_size=0.2, random_state=42):
-    return train_test_split(texts, labels, test_size=test_size, random_state=random_state)
